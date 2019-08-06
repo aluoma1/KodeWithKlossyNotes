1315918d42312917a23f1e387cecdd58a3a66adb
Apple <apple@Apples-MacBook-Pro-13.local>
2019-08-06 07:02:27 -0700

    Initial commit

---
+++ KWKday1.playground/Contents.swift
@@ -0,0 +1,180 @@
+import UIKit
+
+//let greetingSayHello = "Hi!"
+//var nameFirst = "Sheila"
+//
+////string interpolation
+//print("\(greetingSayHello) \(nameFirst)!")
+//
+////greetingSayHello = "Hola!"
+//
+//print(greetingSayHello)
+//print("what's up?!")
+
+//var first = "Karlie"
+//var last = "Kloss"
+//
+//print("\(first)\(last)")
+//print("\(last)\(first)")
+//print("\(first) \(last)")
+//print("\(first) \(last) \(first) \(last)")
+//print("I Love \(first)")
+
+//var integer = 4
+//var double = 5.0
+
+
+
+//print(integer*double)
+//print(Int(double) * integer)
+
+
+//var a = 12
+//var b = 65
+//var c = 31
+//var d = 98
+//
+//print((Double(a + b + c + d)/4))
+
+//var perHour =  10.25
+//var hoursWorked = 20.0
+//
+//print(perHour * Double(hoursWorked))
+
+
+//CONDITIONALS
+ //comparison operators
+//5 < 3
+//12 > 7
+//6 != 8
+//7 == 7
+//"7" == "7"
+//7 == 7.0
+//
+//var luckyNum = 7
+//luckyNum < 10
+
+
+
+//if, else if statements
+
+//var dogAge = 10
+//
+//if dogAge < 2 {
+//    print("You are a puppy 🐶!")
+//}
+//
+//else if dogAge > 12 {
+//    print("You are elderly ❤️")
+//}
+//
+//else {
+//    print("That's ruff!")
+//}
+
+
+
+//var favoriteFood = "Starbucks"
+//
+//if favoriteFood == "Chipotle" {
+//    print("That's gross")
+//}
+//
+//else if favoriteFood == "Starbucks" {
+//    print("MMmm.. okay I get it")
+//}
+//
+//else if favoriteFood == "KFC" {
+//    print("Finger licken' good")
+//}
+//
+//else {
+//    print("cool")
+//
+//}
+
+
+
+//var hasAccount = false
+//
+//if hasAccount == true {
+//    print("Please log in!")
+//}
+//
+//else if hasAccount == false {
+//    print("Please create an account!")
+//}
+
+
+//var grade = 6
+//
+//if grade <= 5 {
+//
+//}
+
+
+//func walkDog() {
+//    print("put leash on dog")
+//    print("walk out door")
+//    print("take dog to park")
+//}
+//
+//walkDog() //called the function named walkDog()
+
+//func bOfC() {
+//    print("pull out bowl, milk, and cereal")
+//    print("put cereal in bowl")
+//    print("put milk in bowl")
+//    print("get spoon")
+//    print("eat")
+//}
+//
+//bOfC()
+
+
+//func walkDog(numberOfDogs : Int) {
+//    print("There are \(numberOfDogs) dogs in the house")
+//}
+//
+//walkDog(numberOfDogs : 4)
+
+
+
+//func walkDog(numberOfDogs : Int) -> Int {
+//
+//    var lengthOfWalk = numberOfDogs * 15
+//
+//    return lengthOfWalk
+//
+//}
+//
+//walkDog(numberOfDogs : 4)
+
+
+//func pbj() {
+//
+//print("go into kitchen")
+//print("get bread")
+//print("get PB")
+//print("get jelly")
+//print("get plate and knife")
+//print("spread PB+J")
+//print("put bread together")
+//print("cut in half")
+//}
+//
+//pbj()
+
+//func pbj(numberOfPbj : Int) {
+//
+//    print("You want me to make \(numberOfPbj) PB+J")
+//}
+//
+//pbj(numberOfPbj: 5)
+
+//func pbj(numberOfPbj : Int) -> Int {
+//    var time = numberOfPbj * 3
+//    return time
+//}
+//
+//pbj(numberOfPbj: 3)
