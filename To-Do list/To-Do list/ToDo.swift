0118bfdee2052aea3168a98313cfbffe4e2632bf
Apple <apple@Apples-MacBook-Pro-13.local>
2019-08-12 07:35:22 -0700

    Second to-do list thing

---
+++ To-Do list/To-Do list/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  To-Do list
+//
+//  Created by Apple on 8/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = " "
+    var important = false
+}
